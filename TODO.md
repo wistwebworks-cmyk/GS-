2f593a5a602d1abc33a86ebcf96f090f564660fb
Wist Webworks <wistwebworks@gmail.com>
2025-10-18 18:19:35 +0600

    Fix responsiveness: Add media queries to style.css for login page and update TODO.md

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO: Make the Whole Web Page Responsive
+
+## Steps to Complete:
+- [x] Add media query for tablets (max-width: 768px): Adjust widths to 90%, reduce paddings/margins/font sizes/images for Profilecard, SideProfilecard, achievementscard, reeldor, cb sections.
+- [x] Add media query for phones (max-width: 480px): Further reduce sizes, stack button groups vertically if needed, ensure text wraps.
+- [x] Test responsiveness: Open fain.html in browser and check on different screen sizes using dev tools.
+- [x] Verify no horizontal overflow and good usability on mobile/tablet.
+- [x] Add responsive design to login page (index.html): Added media queries in style.css for tablets and phones to scale .wp, inputs, buttons, and text.
+- [x] Test login page responsiveness: Open index.html in browser and check on different screen sizes using dev tools.
--- fain.js
+++ fain.js
@@ -9,16 +9,16 @@ let descripTion= document.getElementById("member-desc")
 
 const AllMembers = [
     {
-        name: "Nit The golalu",
-        img: "nit.png",
-        desc:``
+        name: "Nir the Nit(Commander)",
+        img: "nit.jpg",
+        desc:`So, this guy is the commander of Gyatt Society. Yeah, you read it right, this gol alu is the commander, it’s no joke. Whenever you make any joke about his weight, he’ll be like, “Bro, I have lost 1 kg of my weight, my BMI is 24,” like bro doesn’t have 20 kgs of weight in each of his cheeks. And this gol alu is so rich that he pays the full fees of his private tuition but spends the entire batch time playing games in Gamers Arena. And how can we forget about his biggest pride, Reeldior (bro thinks of himself as very tough by watching reels the day before the exam).`
 
 
 
     },
     {
        
-        name: "Ahnaf the JoyBoy(VP)",
+        name: "Ahnaf the Ahanap(VP)",
         img: "Ahnaf.jpg",
         desc: ` Ah.. yes.. this pervy guy here is the vice president of Gyatt Society and one of the founding members. Didn't want to talk about him but yeah had no other choice. Anyways this guy was named Ahanap by and societily we know him as ahanap. A wannabe cool guy, who thinks he has da rizz. No wonder his thoughts got him a Joke D'or on 2025. People still talk about how he fell on the ground while the "Dor lagse" run.. LMAO.`
 
@@ -27,7 +27,7 @@ const AllMembers = [
         
         name: "Pranto The Lil Sheikh",
         img: "pranto.png",
-        desc: `klk lehum`
+        desc: `So, this is the Sheikh of our society. If you see his pfp, it might not look like it but he is. But I need to warn you about one thing, NEVER MESS WITH HIM because he will literally print you. Though there are many rumors about him being a vondo sheikh, giringibaj, etc., there are also many good traits of him like he is a calculator, printer, and of course, the topper of the society.`
     },
     {
        
@@ -36,44 +36,47 @@ const AllMembers = [
         desc:`OMG, it's time for jimmy?? Oh man. Lean in and lend me your ears, this guy dangerous. 
                 Words about him shouldn't go outside.   
                 Jim also known as jimmy is a guy who formerly used to take backshot from Gyatt Society's one of the main antagonist Devashish.
-                 Though, he replaced Devashish with Reza as devashish couldn't keep up.
-                  He's also the guy who gets bullied the most in the group and so.. he throws some elixir (for him) called pussy juice.
-                   And his "ThuThu" killer technique is still feared by many.
-                   Well, they say everyone has a weakness for him it's Chemistry. 
+                Though, he replaced Devashish with Reza as devashish couldn't keep up.
+                He's also the guy who gets bullied the most in the group and so.. he throws some elixir (for him) called pussy juice.
+                And his "ThuThu" killer technique is still feared by many.
+                Well, they say everyone has a weakness for him it's Chemistry. 
                 Never take the name of chemistry in front of him, otherwise you would be covered in dung. Eww`},
     {
        
-        name: "Minhaj The Great",
+        name: "Minhaj The Kalua",
         img: "minhaj.png",
-        desc:``
+        desc:`Here is the footballer guy of our society. Yeah, the biggest fan of Ronaldo after Speed.
+Like bro, if you just say the word “football” in front of him, he will instantly forget about everything else going on in the world. And this tall nigga’s legendary pic “Ass ta shei to” is also the pfp of our IG group. He is also a Reeldior superstar, I mean a reel addict who will spam random memes in the middle of a serious convo.`
     },
     {
        
-        name: "Mahdi The Legend",
+        name: "Mahdi The Dihh",
         img: "mahdi.png",
-        desc:``
+        desc:`Mahdihh. The dihh in his name stands for his small black cute dick. He's a tsundere guy who thinks he's a nonchalant shit.`
     },
     {
-        name: "Tasin The Boss",
+        name: "Tasin The Sauce",
         img: "tasin.png",
-        desc:``
+        desc:`The mascot of Gyatt Society. He has GYATTT!!! Though it reeks bad, ewww. He was once a innocent and good guy who used to have periods on a regular basis. But now he tryna play cool. ;-; But anyways GYATTTT!!!`
     },
     {
        name: "The Vodka Abid",
         img: "vodka.jpg",    
-        desc:``
+        desc:`Everyone, here we present to you the selev of Ms. Jhora — sorry, I mean bf of Ms. Jhora, the Vodka Abid. You might be wondering why we are calling him Vodka, though he is not that fat, he is Vodka, he just is.
+              And when we are talking about this jhoraitta, I mean jhograitta guy,  how can we forget about his biggest trait: “Challuness.”
+              Yeah, he no doubt is the challuest guy in the society.`
 
     }, 
        
     {
-        name: "Said The Said",
+        name: "Said The Backshot",
         img: "said.png",    
-        desc:``
+        desc:`Ah, ladies and gentlemen, gather around, for today I present to you the product of Ahanap and Suiii, the one and only Said. A little naughty, little good, below average guy who likes to take it in the back. We still remember that moment at metro station when he almost got slapped by an aunty, LOL(be more careful son.) But bro is kinda cute tho. Literally a bbg.`
     },
     {
-         name: "Araf The Coder", 
+         name: "Araf The Crafty", 
         img: "araf.png",
-        desc:``
+        desc:`Araf Sensei is one of the ogest and loyal member of Gyatt Society. Have always been useful in the society. Gyatt Society is proud of you.`
     },  
     {
         name: "Fatin the fat",
@@ -128,7 +131,7 @@ const AllSideMembers = [
     {
         name: "The Lomba Abid",
         img: "lomba.png",
-        desc:``
+        desc:`Yeah, the Lomba Abid aka the gyani wannabe guy. Bro will be saying, “Man, nowadays I am studying 24/7,” and the very next minute you’ll see him texting the 69th girl in his inbox. He is also known for his rivalry against the Challu. Though he was once a member of the society, he bijoyed us in the great battle against the L Society. But after that, he still hangs out with us, so he is a side character.`
 
 
 
@@ -137,20 +140,21 @@ const AllSideMembers = [
        
         name: "Raiyan the Shingara",
         img: "raiyan.jpeg",
-        desc: ` `
+        desc: `Here comes the shingara guy. Oops, I mean Raiyan. Yeah, the inventor of the legendary INIK! PINIKKKKKK! Also the most confident liar one. Oh, how can we forget that he is also the fake don of the society. Now, you might be wondering why I called him shingara at first yeah, because whenever we are talking about Mahbub Sir, who is famous for his cruelty, he’ll be like, “Bro, just shout shingara in front of him, he’ll be scared to death.”
+Bro says all this nonsense without any logic. Though he is not an official member of the society, he hangs out with us sometimes, so he is a side character.`
 
     },
     {
         
-        name: "The Helecopter",
+        name: "The Helicopter",
         img: "helec.jpeg",
-        desc: ``
+        desc: `The Helicopter which is the Bell-407 patrols everyday and keeps track of Gyatt Society's future plans and more.`
     },
     {
        
-        name: "Devasish the Don",
+        name: "Devashish the Don",
         img: "devasish.jpeg",
-        desc:``},
+        desc:`One of the main antagonists of Gyatt Society. He is accused of being the pilot of The Helicopter which patrols everyday and keeps track of Gyatt Society. Executives consider Devashish as one of the greatest threats to Gyatt Society.`},
     {
        
         name: "The Diddy Dehan",
